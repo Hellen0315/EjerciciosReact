5806ff79dd1f757aafa45afc48d2d1f57101dce5
Hellen0315 <maryavril315@gmail.com>
2020-08-22 00:54:57 -0500

    update app

--- scripts/App.js
+++ scripts/App.js
@@ -19,12 +19,55 @@ class App extends React.Component {
     })
   };
 
+  filtroCountry = (data) => () => {
+    const dataA = this.state.dataHoteles;
+    dataA.filter(datos => datos.country === data)
+    this.setState({
+      dataHoteles:dataA
+    });
+  };
+
+  filtroPrices = (data) => () => {
+    const dataA = this.state.dataHoteles;
+    dataA.filter(datos => datos.price === data)
+    this.setState({
+      dataHoteles: dataA
+    });
+  };
+
+  filtroSize = (data) => () => {
+    const dataA = this.state.dataHoteles;
+    if (data == "pequeño"){
+      dataA.filter(room => room.rooms <= 10);
+     } 
+    if (data == "mediano"){
+      dataA.filter(room => room.rooms > 10 && room.rooms <= 20);
+    }
+    if (data == "grande"){
+      dataA.filter(room => room.rooms > 20); 
+    }   
+    this.setState({
+      dataHoteles: dataA
+    });
+  };
   render() {
+    const {priceFilter,  countryFilter, sizeFilter} = this.state.filters
+     if (countryFilter != "Todos"){
+        this.filtroCountry(countryFilter);
+     }
+     if (priceFilter != "Todos"){
+       this.filtroPrices(priceFilter);
+     }
+     if (sizeFilter != "Todos"){
+       this.filtroSize(sizeFilter);
+     }
+     console.log(this.state.dataHoteles);
+     console.log(priceFilter);
     return (
       <div className="App">
         <Header initialDate={this.state.filters.initialDate}  finalDate={this.state.filters.finalDate}/>
         <Filter data={this.state.dataHoteles} {...this.state.filters} onChange={this.handleonChage} />
-        <HotelContainer data={this.state.dataHoteles} {...this.state.filters}/>
+        <HotelContainer data={this.state.dataHoteles}/>
       </div>
     );
   }
--- scripts/HotelContainer.js
+++ scripts/HotelContainer.js
@@ -1,57 +1,9 @@
 class HotelContainer extends React.Component {
-    state={
-      arrayEnd:this.props.data
-    }
-
-    filtroCountry = (data) => () => {
-      const dataA = this.state.arrayEnd;
-      dataA.filter(datos => datos.country === data)
-      this.setState({
-        previousMessages: dataA
-      });
-    };
-
-    filtroPrices = (data) => () => {
-      const dataA = this.state.arrayEnd;
-      dataA.filter(datos => datos.price === data)
-      this.setState({
-        previousMessages: dataA
-      });
-    };
-
-    filtroSize = (data) => () => {
-      const dataA = this.state.arrayEnd;
-      if (data == "pequeño"){
-        dataA.filter(room => room.rooms <= 10);
-       } 
-      if (data == "mediano"){
-        dataA.filter(room => room.rooms > 10 && room.rooms <= 20);
-      }
-      if (data == "grande"){
-        dataA.filter(room => room.rooms > 20); 
-      }   
-      this.setState({
-        previousMessages: dataA
-      });
-    };
-  
-   render() {
-
-     const {priceFilter,  countryFilter, sizeFilter} = this.props
-     if (countryFilter != "Todos"){
-        this.filtroCountry(countryFilter);
-     }
-     if (priceFilter != "Todos"){
-       this.filtroPrices(priceFilter);
-     }
-     if (sizeFilter != "Todos"){
-       this.filtroSize(sizeFilter);
-     }
-     console.log(this.state.arrayEnd);
-     console.log(priceFilter);
+  render() {    
+    const {data} = this.props;
     return (
       <div className="hotelList">
-      {this.state.arrayEnd.map(hotel => (
+      {data.map(hotel => (
         <CardHotel key={hotel.slug} {...hotel} />
       ))}
       </div>
