2433ec487fe2ef281f93f101ae5ea99787bef2d0
Hellen0315 <maryavril315@gmail.com>
2020-08-26 09:15:16 -0500

    filtros

--- scripts/App.js
+++ scripts/App.js
@@ -19,70 +19,39 @@ class App extends React.Component {
     })
   };
 
-  filterHotels = () => {
-    const { priceFilter, countryFilter, sizeFilter } = this.state.filters;
-    let hotels = this.state.dataHoteles;
-    if (countryFilter !== "Todos") {
-      hotels = hotels.filter((hotel) => hotel.country === countryFilter);
+  filtroHotels = () => {
+    const {priceFilter, countryFilter, sizeFilter} = this.state.filters;
+    let hoteles = this.state.dataHoteles;
+
+    if (priceFilter !=="Todos"){
+      hoteles = hoteles.filter((hotel) => hotel.price == priceFilter);
     }
-    if (priceFilter !== "Todos") {
-      hotels = hotels.filter((hotel) => hotel.price == priceFilter);
+    if (countryFilter !=="Todos"){
+      hoteles = hoteles.filter((hotel) => hotel.country === countryFilter);
     }
-    if (sizeFilter  == "pequeño") {
-      hotels = hotels.filter(room => room.rooms <= 10);
+    if (sizeFilter == "pequeño"){
+      hoteles = hoteles.filter(room => room.rooms <= 10);
     }
-    if (sizeFilter  == "mediano") {
-      hotels = hotels.filter(room => room.rooms > 10 && room.rooms <= 20);
+    if (sizeFilter == "mediano"){
+      hoteles = hoteles.filter(room => room.rooms > 10 && room.rooms <= 20);
     }
-    if (sizeFilter  == "grande") {
-      hotels = hotels.filter(room => room.rooms > 20);
+    if (sizeFilter == "grande"){
+      hoteles = hoteles.filter(room => room.rooms > 20);
     }
-    return hotels
+    return hoteles
   }
 
-  filtroCountry = () => {
-    const { countryFilter } = this.state.filters;
-    if (countryFilter != "Todos") {
-      const dataA = this.state.dataHoteles;
-      const dataC = dataA.filter(datos => datos.country === data)
-
-      console.log(dataC, data);
-
-      this.setState({
-        dataHoteles: dataC
-      });
-    }
-  };
-
-  filtroPrices = (data) => {
-    const dataA = this.state.dataHoteles;
-    dataA.filter(datos => datos.price === data)
-    this.setState({
-      dataHoteles: dataA
-    });
-  };
-
-  filtroSize = (data) => () => {
-    const dataA = this.state.dataHoteles;
-   
-    this.setState({
-      dataHoteles: dataA
-    });
-  };
-
-  render() {
+  render (){
     return (
       <div className="App">
-        <Header initialDate={this.state.filters.initialDate} finalDate={this.state.filters.finalDate} />
+        <Headers  initialDate={this.state.filters.initialDate} finalDate={this.state.filters.finalDate}/>
         <Filter data={this.state.dataHoteles} {...this.state.filters} onChange={this.handleonChage} />
-        <HotelContainer data={this.filterHotels()} />
+        <HotelContainer  data = {this.filtroHotels()} />
       </div>
     );
   }
 }
 
 const rootElement = document.getElementById("app");
-ReactDOM.render(
-  <App />,
-  rootElement);
+ReactDOM.render(<App />, rootElement);
 
